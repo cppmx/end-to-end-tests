a4e18ce0a9778feb894c75f12142758ae145ae6b
Carlos Colon <espacio.sideral@gmail.com>
2020-05-03 09:43:49 -0500

    :computer: Run this BASH script

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -it -v ${pwd}/tests/:/tests:ro -v ${pwd}/results:/results:rw carlos3d/end-to-end-tests --outputdir /results --variable BROWSER:chrome /tests
+docker run --rm -it -v ${pwd}/tests/:/tests:ro -v ${pwd}/results:/results:rw cppmx/end-to-end-tests --outputdir /results --variable BROWSER:chrome /tests
